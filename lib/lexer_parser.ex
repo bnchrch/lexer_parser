7e60f25e5097af7c7171d71da9727b01913fcbd3
Ben Church <ben.church@metalab.co>
2019-01-14 22:34:45 -0700

    Initial Commit

---
+++ lib/lexer_parser.ex
@@ -0,0 +1,18 @@
+defmodule LexerParser do
+  @moduledoc """
+  Documentation for LexerParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LexerParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lexer_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule LexerParserTest do
+  use ExUnit.Case
+  doctest LexerParser
+
+  test "greets the world" do
+    assert LexerParser.hello() == :world
+  end
+end
